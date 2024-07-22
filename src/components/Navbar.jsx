22e651c987733e64299a43632cba9c4921d82a18
code4learnn <158156238+code4learnn@users.noreply.github.com>
2024-07-23 00:23:57 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
                         <a className='hover:font-bold' href='#'>Contact</a>
                     </li>
                 </ul> */}
-                <button className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'>
+                <button onClick className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'>
                     <img className='invert  w-10 p-1' src="/icons/github.svg" alt="github logo" />
                     <span className='font-bold px-2'>GitHub</span>
 

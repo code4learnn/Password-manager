d9d6e13e2146c380b319d269ba2232b09e87dbb2
code4learnn <158156238+code4learnn@users.noreply.github.com>
2024-07-22 10:12:15 +0530

    Added Backend

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,16 @@ function App() {
 
   return (
     <>
+    <div className="cont top-0 right-0">
     <Navbar/>
-
-    <div className='min-h-[87vh]  bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>
+    </div>
+    <div className='bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]'>
      <Manager/>
 
     </div>
-     <Footer/>
+    <div className=' inset-x-0 bottom-0 h-16 ...'>
+      <Footer/>
+    </div>
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Footer=()=>{
     return(
-        <div className="bg-slate-800 text-white flex flex-col justify-center items-center">
+        <div className="bg-slate-800 text-white flex flex-col justify-center items-center ">
             <div className="logo font-bold  text-white text-2xl">
         <span className='text-green-700'>/&lt;</span>
             Pass
@@ -9,7 +9,7 @@ const Footer=()=>{
             
             </div>
             <div className="flex justify-right items-right  ">
-            Made by <img  className="w-7 mx-2 py-3" src="icons/heart.png" alt="" /> by code4learnn
+            Made by <img  className=" w-7 mx-2  " src="icons/heart.png" alt="" /> by code4learnn
             </div>
         </div>
     )

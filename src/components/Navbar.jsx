2f6c8569d3e2801c312ecf3af396e695d8b6c4b6
code4learnn <158156238+code4learnn@users.noreply.github.com>
2024-08-14 19:37:58 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Navbar = () => {
+    const onclick=()=>{
+        window.location.href='https://github.com/code4learnn'
+    }
     return (
         <nav className='bg-slate-800 text-white '>
             <div className="mycontainer flex justify-between items-center px-4 py-5 h-14">
@@ -16,7 +19,7 @@ const Navbar = () => {
                         <a className='hover:font-bold' href='#'>Contact</a>
                     </li>
                 </ul> */}
-                <button onClick className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'>
+                <button onClick={onclick} className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'>
                     <img className='invert  w-10 p-1' src="/icons/github.svg" alt="github logo" />
                     <span className='font-bold px-2'>GitHub</span>
 
